e7bf4be34be8fe5ab276c7be30f6ed3658f64804
Roman Shipiev <roman@shipiev.pro>
2014-09-19 16:31:47 +0400

    +relation_name, +relation_options

--- lib/acts_as_journalized/acts_as_journalized.rb
+++ lib/acts_as_journalized/acts_as_journalized.rb
@@ -7,14 +7,21 @@ module Redmine
       end
 
       module ClassMethods
-        def acts_as_activity_provider(options = {})
+        def acts_as_journalized(options = {})
           cattr_accessor :journalized_options
 
-          options.assert_valid_keys(:excepted_attributes)
-          self.journalized_options = {excepted_attributes: [:updated_at, :updated_on] }
+          options.assert_valid_keys(:excepted_attributes, :relation_name, :relation_options)
+          self.journalized_options = {
+              excepted_attributes: [:updated_at, :updated_on],
+              relation_name: :journals,
+              relation_options: {}
+          }
           self.journalized_options.merge(options)
 
-          has_many :journals, as: :journalized, dependent: :destroy
+          relation_options = {class_name: 'Journal', as: :journalized, dependent: :destroy}.
+              merge(self.journalized_options[:relation_options])
+
+          has_many self.journalized_options[:relation_name], relation_options
 
           send :include, Redmine::Acts::Journalized::InstanceMethods
 
@@ -27,12 +34,16 @@ module Redmine
           base.extend ClassMethods
         end
 
+        def init_journal(user = User.current, notes = '')
+          @journal ||= send(self.journalized_options[:relation_name]).build(user: user, notes: notes)
+        end
+
         def excepted_attributes
           self.class.journalized_attributes[:excepted_attributes]
         end
 
         def journalize_attributes(user = User.current, notes = '')
-          @journal ||= journals.build(user: user, notes: notes)
+          init_journal(user: user, notes: notes)
           changes.except(*excepted_attributes).each_pair do |column, values|
             @journal.details.build(property: 'attr', prop_key: column, old_value: values.first, value: values.last)
           end
