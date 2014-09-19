e54d4c661299d46f9b124b7e72b7a2c6cb3e622a
Roman Shipiev <roman@shipiev.pro>
2014-09-19 17:07:22 +0400

    fix: init journalized_options

--- lib/acts_as_journalized/acts_as_journalized.rb
+++ lib/acts_as_journalized/acts_as_journalized.rb
@@ -13,15 +13,14 @@ module Redmine
           options.assert_valid_keys(:excepted_attributes, :relation_name, :relation_options)
           self.journalized_options = {
               excepted_attributes: [:updated_at, :updated_on],
-              relation_name: :journals,
+              relation_name: 'journals',
               relation_options: {}
-          }
-          self.journalized_options.merge(options)
+          }.merge(options)
 
           relation_options = {class_name: 'Journal', as: :journalized, dependent: :destroy}.
               merge(self.journalized_options[:relation_options])
 
-          has_many self.journalized_options[:relation_name], relation_options
+          has_many self.journalized_options[:relation_name].to_sym, relation_options
 
           send :include, Redmine::Acts::Journalized::InstanceMethods
 
